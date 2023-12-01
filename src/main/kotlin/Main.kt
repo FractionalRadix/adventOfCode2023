2fa951b275a499ce289f219d407dd14d307fdf59
Serge <nerostjust@gmail.com>
2023-12-01 21:16:40 +0100

    Add solution for second part.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,21 +6,23 @@ fun main(args: Array<String>) {
 }
 
 fun solveDay01() {
-    val firstInputAsStrings = Path("""src/main/resources/inputFiles/AoCDay01_sample1.txt""").readLines()
-
+    val firstInputAsStrings = Path("""src/main/resources/inputFiles/AoCDay01.txt""").readLines()
     val values1 = firstInputAsStrings
         .filter { str -> str.isNotEmpty() }
         .sumOf { str -> calibrationValue(str) }
+    println("Answer for the first part: $values1.") // 55447
 
-    println("Answer for the first part: $values1.")
-
-    val secondInputAsStrings = Path("""src/main/resources/inputFiles/AoCDay01_sample2.txt""").readLines()
-    val values2 = secondInputAsStrings
+    val secondInputAsStrings = Path("""src/main/resources/inputFiles/AoCDay01.txt""").readLines()
+    val firstDigits = secondInputAsStrings
         .filter { str -> str.isNotEmpty() }
-        .map { str -> replaceWordsWithDigits(str) }
-        .sumOf { str -> calibrationValue(str) }
-
-    println("Answer for the second part: $values2.")
+        .map { str -> firstWordOrDigit(str) }
+    val secondDigits = secondInputAsStrings
+        .filter { str -> str.isNotEmpty() }
+        .map { str -> lastWordOrDigit(str) }
+    val values2 = firstDigits
+        .zip(secondDigits) { a,b -> 10 * a + b }
+        .sum()
+    println("Answer for the second part: $values2") // 54706
 }
 
 fun calibrationValue(str: String): Int {
@@ -30,18 +32,71 @@ fun calibrationValue(str: String): Int {
     val lastDigit = str
         .last { ch -> ch.isDigit() }
         .digitToInt()
-    println("$firstDigit $lastDigit")
     return ( 10 * firstDigit + lastDigit )
 }
 
-//TODO!-
-fun replaceWordsWithDigits(str: String) = str
-        .replace("one", "1")
-        .replace("two", "2")
-        .replace("three", "3")
-        .replace("four", "4")
-        .replace("five", "5")
-        .replace("six", "6")
-        .replace("seven", "7")
-        .replace("eight", "8")
-        .replace("nine", "9")
+/** Get the first digit from a string, whether it is spelled out or just given as a digit.
+ * Naive but functional solution.
+ * @param str A String that contains at least one digit, either as a digit or spelled out in letters.
+ * @return The value of the first digit in the input.
+ */
+fun firstWordOrDigit(str: String): Int {
+    if (str.startsWith("zero") or str.startsWith("0"))
+        return 0
+
+    if (str.startsWith("one") or str.startsWith("1"))
+        return 1
+    if (str.startsWith("two") or str.startsWith("2"))
+        return 2
+    if (str.startsWith("three") or str.startsWith("3"))
+        return 3
+
+    if (str.startsWith("four") or str.startsWith("4"))
+        return 4
+    if (str.startsWith("five") or str.startsWith("5"))
+        return 5
+    if (str.startsWith("six") or str.startsWith("6"))
+        return 6
+
+    if (str.startsWith("seven") or str.startsWith("7"))
+        return 7
+    if (str.startsWith("eight") or str.startsWith("8"))
+        return 8
+    if (str.startsWith("nine") or str.startsWith("9"))
+        return 9
+
+    return firstWordOrDigit(str.substring(1))
+}
+
+/** Get the last digit from a string, whether it is spelled out or just given as a digit.
+ * Naive but functional solution.
+ * @param str A String that contains at least one digit, either as a digit or spelled out in letters.
+ * @return The value of the last digit in the input.
+ */
+fun lastWordOrDigit(str: String): Int {
+    if (str.endsWith("zero") or str.endsWith("0"))
+        return 0
+
+    if (str.endsWith("one") or str.endsWith("1"))
+        return 1
+    if (str.endsWith("two") or str.endsWith("2"))
+        return 2
+    if (str.endsWith("three") or str.endsWith("3"))
+        return 3
+
+    if (str.endsWith("four") or str.endsWith("4"))
+        return 4
+    if (str.endsWith("five") or str.endsWith("5"))
+        return 5
+    if (str.endsWith("six") or str.endsWith("6"))
+        return 6
+
+    if (str.endsWith("seven") or str.endsWith("7"))
+        return 7
+    if (str.endsWith("eight") or str.endsWith("8"))
+        return 8
+    if (str.endsWith("nine") or str.endsWith("9"))
+        return 9
+
+    return lastWordOrDigit(str.substring(0, str.length - 1))
+}
