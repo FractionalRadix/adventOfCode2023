cfbd1807a861226310d350d68cd8821469074f03
Serge <nerostjust@gmail.com>
2023-12-09 16:18:27 +0100

    Add solutions for day 6.

---
+++ src/main/kotlin/com/cormontia/adventOfCode/year2023/Day05Solver.kt
@@ -0,0 +1,133 @@
+package com.cormontia.adventOfCode.year2023
+
+import com.cormontia.adventOfCode.year2023.utils.parseListOfLongs
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+class Day05Solver {
+
+    fun solve() {
+        val input = Path("""src/main/resources/inputFiles/AoCDay05_sample1.txt""")
+            .readLines()
+
+        val seedsStr = input[0].split(":")
+        println(seedsStr)
+        val seeds = parseListOfLongs(seedsStr[1])
+
+        println(seeds)
+
+        val transformers = parseTransformationFunctions(input)
+
+        val soils = mutableListOf<Long>()
+        for (seed in seeds) {
+            var value = seed
+            println("seed: $value")
+            for (transformer in transformers) {
+                value = transformer.transform(value)
+                print(" -> $value ")
+            }
+            println(" -> $value")
+            soils.add(value)
+        }
+
+        println("The lowest soil nr is ${soils.min()}")
+
+
+        var sourceRanges = mutableListOf<LongRange>()
+        for (i in seeds.indices step 2) {
+            sourceRanges.add(LongRange(seeds[i], seeds[i] + seeds[i+1] - 1))
+        }
+        println(sourceRanges)
+
+        // First, the naive solution:
+        val finalMapping = mutableMapOf<Long,Long>()
+        for (sourceRange in sourceRanges) {
+            for (source in sourceRange) {
+
+                var tmp = source
+                for (mapper in transformers) {
+                    tmp = mapper.transform(tmp)
+                }
+                finalMapping[source] = tmp
+
+            }
+        }
+        val answer = finalMapping.map { it.value }.min()
+        println("Lowest location number: $answer")
+
+
+    }
+
+    /**
+     * Given a range, and a list of non-overlapping ranges, split the range according to these non-overlapping ranges.
+     * For example, given [12..22] and the list [10..12], [15..17], [24..29]:
+     * We'd get [12..12], [13..14], [15..17], [18..22].
+     */
+    private fun split(source: LongRange, ranges: List<LongRange>): List<LongRange> {
+        TODO()
+    }
+
+    /**
+     * Given a number and a set of non-overlapping ranges, determine if the number is inside any of these ranges.
+     * If so, return the applicable range; otherwise return <code>null</code>.
+     * @param number The number for which we want to know if it is in any of the ranges.
+     * @param ranges A list of non-overlapping ranges.
+     * @return The range that the number is in, if any; <code>null</code> otherwise.
+     */
+    private fun insideRange(number: Long, ranges: List<LongRange>): LongRange? = ranges.firstOrNull { it.contains(number) }
+
+
+    private fun parseTransformationFunctions(input: List<String>): MutableList<Transformer> {
+        val transformers = mutableListOf<Transformer>()
+        var transformer: Transformer? = null
+        for (i in 1..<input.size) {
+            if (input[i].isEmpty())
+                continue
+            if (input[i].endsWith("map:")) {
+                if (transformer != null) {
+                    transformers.add(transformer)
+                }
+                transformer = Transformer()
+            } else {
+                val longs = parseListOfLongs(input[i])
+                val range = TransformRange(longs[0], longs[1], longs[2])
+                transformer?.ranges?.add(range)
+            }
+        }
+        transformers.add(transformer!!)
+        return transformers
+    }
+
+    data class TransformRange(
+        val destination: Long,
+        val source: Long,
+        val range: Long,
+    )
+
+    /**
+     * A set of mappings, from seed-to-soil, or soil-to-fertilizer, or fertilizer-to-water, etc.
+     */
+    class Transformer() {
+        val ranges = mutableListOf<TransformRange>()
+
+        fun transform(input: Long): Long {
+            for (range in ranges) {
+                if (input >= range.source && input < range.source + range.range) {
+                    val delta = input - range.source
+                    return range.destination + delta
+                }
+            }
+            return input
+        }
+
+        fun print( ) {
+            println()
+            for (range in ranges) {
+                println("${range.destination} ${range.source} ${range.range}")
+            }
+        }
+    }
+
+
+
+}
---
+++ src/main/kotlin/com/cormontia/adventOfCode/year2023/Day06Solver.kt
@@ -0,0 +1,79 @@
+package com.cormontia.adventOfCode.year2023
+
+import com.cormontia.adventOfCode.year2023.utils.parseListOfInts
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+class Day06Solver {
+    fun solve() {
+        val input = Path("""src/main/resources/inputFiles/AoCDay06.txt""")
+            .readLines()
+            .filter { it.isNotBlank() }
+
+        // Part 1.
+        val timesString = input[0].dropWhile { it != ':' }.drop(1)
+        println("Times are: $timesString")
+        val distancesString = input[1].dropWhile { it != ':' }.drop(1)
+        println("Distances are: $distancesString")
+
+        val times = parseListOfInts(timesString)
+        println(times)
+        val distances = parseListOfInts(distancesString)
+        println(distances)
+
+        val product = solvePart1(times, distances)
+        println("The product of the number of winning options is $product.")
+
+        // Part 2.
+        val timeString = input[0].dropWhile { it != ':' }.drop(1).filter { !it.isWhitespace() }
+        val distanceString = input[1].dropWhile { it != ':' }.drop(1).filter { !it.isWhitespace() }
+        println("Single time: $timeString")
+        println("Single distance: $distanceString")
+        val time = timeString.toLong()
+        println("Time as long: $time")
+        val distance = distanceString.toLong()
+        println("Distance as long: $distance")
+
+        val answer = countOptions(time, distance)
+        println("Nr of options: $answer")
+    }
+
+    private fun solvePart1(times: List<Int>, distances: List<Int>): Int {
+        var product = 1
+        for (i in times.indices) {
+            val time = times[i]
+            val targetDistance = distances[i]
+            val nrOfOptions = countOptions(time, targetDistance)
+            println("Nr of options: $nrOfOptions")
+            product *= nrOfOptions
+        }
+        return product
+    }
+
+    private fun countOptions(time: Int, targetDistance: Int): Int {
+        var count = 0
+        for (i in 0 .. time) {
+            val speed = i
+            val remainingTime = time - i
+            val distance = speed * remainingTime
+            if (distance > targetDistance) {
+                count++
+            }
+        }
+        return count
+    }
+
+    private fun countOptions(time: Long, targetDistance: Long): Long {
+        var count = 0L
+        for (i in 0 .. time) {
+            val speed = i
+            val remainingTime = time - i
+            val distance = speed * remainingTime
+            if (distance > targetDistance) {
+                count++
+            }
+        }
+        return count
+    }
+
+}
