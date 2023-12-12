8f34eea3e11e17660c1a4b9572f4b3d1e113d5d2
Serge <nerostjust@gmail.com>
2023-12-12 08:07:16 +0100

    Add another part of the solution for day 12, part 1.

--- src/main/kotlin/com/cormontia/adventOfCode/year2023/Day12Solver.kt
+++ src/main/kotlin/com/cormontia/adventOfCode/year2023/Day12Solver.kt
@@ -13,7 +13,7 @@ class Day12Solver {
         for (line in input) {
             val parts = line.split(" ")
             val values = parseListOfLongs(parts[1], ",")
-            val myLine = Line(parts[0], values)
+            val myLine = Line(parts[0], values.toMutableList())
             myLine.print()
             myLine.splitBlocks()
             println()
@@ -21,7 +21,7 @@ class Day12Solver {
 
     }
 
-    class Line(private val str: String, private val values: List<Long>) {
+    class Line(private val str: String, private var values: MutableList<Long>) {
         private var blocks = mutableListOf<String>()
 
         fun print() {
@@ -31,6 +31,24 @@ class Day12Solver {
         fun splitBlocks() {
             blocks = str.split(".").filter { it.isNotBlank() }.toMutableList()
             println(blocks)
+
+            //NOTE. If the first block starts with a '#', or the last one ends with a '#', you know you've fixed that contiguous block.
+            // Then there is only one way that block could fit, and you can remove that part.
+
+            if (blocks[0].startsWith('#')) {
+                blocks[0] = blocks[0].substring(values[0].toInt()) //TODO!+ Check for off-by-one error.
+                //TODO!~  Also remove the block entirely, if it is the empty string.
+                values.removeFirst()
+                println("New blocks: $blocks $values")
+            }
+            if (blocks.last().endsWith('#')) {
+                val block = blocks.last().reversed().substring(values.last().toInt()).reversed()
+                blocks[blocks.size - 1] = block
+                //TODO!~  Also remove the block entirely, if it is the empty string.
+                values.removeLast()
+                println("New blocks: $blocks $values")
+            }
+
         }
     }
 }
