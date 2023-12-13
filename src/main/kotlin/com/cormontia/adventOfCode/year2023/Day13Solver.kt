a006499bd2eee65ff92276a424341b4915d8cd11
Serge <nerostjust@gmail.com>
2023-12-13 18:48:54 +0100

    Add more code for solution for day 13, part 2. (Not yet correct).

--- src/main/kotlin/com/cormontia/adventOfCode/year2023/Day13Solver.kt
+++ src/main/kotlin/com/cormontia/adventOfCode/year2023/Day13Solver.kt
@@ -3,10 +3,11 @@ package com.cormontia.adventOfCode.year2023
 import utils.transposeStringList
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
+import kotlin.math.abs
 
 class Day13Solver {
     fun solve() {
-        val input = Path("""src/main/resources/inputFiles/AoCDay13.txt""")
+        val input = Path("""src/main/resources/inputFiles/AoCDay13_sample1.txt""")
             .readLines()
 
         val maps = mutableListOf<Map>()
@@ -23,6 +24,109 @@ class Day13Solver {
         val map = Map(current)
         maps.add(map)
 
+        solvePart1(maps)
+
+        solvePart2(maps) // 21186 is TOO LOW. // 31399 is TOO HIGH.
+    }
+
+    private fun solvePart2(maps: MutableList<Map>) {
+
+        var horizontalSum = 0
+        var verticalSum = 0
+        for (map in maps) {
+
+            println()
+            println("NEW MAP.")
+
+            val horiz = findSmudgeLine(map.list)
+            horizontalSum += horiz.sum()
+
+            val vertic = findSmudgeLine(transposeStringList(map.list))
+            verticalSum += vertic.sum()
+        }
+
+        println("Horizontal sum: $horizontalSum")
+        println("Vertical sum: $verticalSum")
+        println("Result: ${100 * horizontalSum + verticalSum}")
+    }
+
+    /**
+     * Given two strings of equal length, count in how many places they differ.
+     * @param str1 A string.
+     * @param str2 A string whose length is equal to str1.
+     * @return The number of places in which the first input string differs from the second one.
+     */
+    private fun differences(str1: String, str2: String): Int {
+        var differences = 0
+        for (i in str1.indices) {
+            if (str1[i] != str2[i]) {
+                differences++
+            }
+        }
+        return differences
+    }
+
+
+    fun findSmudgeLine(list: List<String>): List<Int> {
+
+        // First, find all lines that differ in exactly one spot.
+        var pairs = mutableListOf<Pair<Int,Int>>()
+        for (i in list.indices) {
+            for (j in i + 1 ..< list.size) {
+                val numDiff = differences(list[i], list[j])
+                if (numDiff == 1) {
+                    pairs.add(Pair(i,j))
+                }
+            }
+        }
+
+        // For every pair, determine where the mirroring axis would be: exactly between them.
+        // First, the difference between them should be an odd number.
+        pairs = pairs.filter { isOdd(Math.abs(it.first - it.second)) }.toMutableList()
+
+        // For these candidates, find the axis.
+        // Then check if all OTHER lines mirror properly around that axis.
+        val validAxes = mutableListOf<Int>()
+        for (pair in pairs) {
+            val axis = (pair.first + pair.second + 1)/2
+            val axisValid = isValidAxis(axis, list, pair)
+            if (axisValid) {
+                println("Valid! $axis")
+                validAxes.add(axis)
+            }
+        }
+
+        println("Axes: $validAxes" )
+        return (validAxes)
+    }
+
+    private fun isValidAxis(
+        axis: Int,
+        list: List<String>,
+        pair: Pair<Int, Int>
+    ): Boolean {
+        var axisValid = true
+        var d = 1
+        while (axis - d >= 0 && axis + 1 + d < list.size) {
+            val idx1 = axis - d
+            val idx2 = axis - 1 + d
+            //println("axis=$axis d=$d idx1==$idx1 idx2==$idx2")
+            if (idx1 == Math.min(pair.first, pair.second)) { // Skip the line that differs by one.
+                d++
+                continue
+            }
+            val diff = differences(list[idx1], list[idx2])
+            if (diff != 0) {
+                axisValid = false
+            }
+            d++
+        }
+        return axisValid
+    }
+
+    fun isOdd(n: Int): Boolean = (n % 2) == 1
+
+    private fun solvePart1(maps: MutableList<Map>) {
         var horizontalSum = 0
         var verticalSum = 0
         for (m in maps) {
@@ -64,7 +168,7 @@ class Day13Solver {
             return result
         }
 
-        private fun confirmHorizontalMirroring(list: List<String>, i: Int, j: Int): Boolean {
+        fun confirmHorizontalMirroring(list: List<String>, i: Int, j: Int): Boolean {
             var d = 1
             while (i - d >= 0 && j + d < list.size) {
                 val strI = list[i - d]
