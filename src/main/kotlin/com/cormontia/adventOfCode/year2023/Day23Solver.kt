f85f063dd48df91423224d203b57c3fa6360c428
Serge <nerostjust@gmail.com>
2023-12-23 18:47:30 +0100

    Add sample data for day 23. Add skeleton code for days 23, 24, and 25.

---
+++ src/main/kotlin/com/cormontia/adventOfCode/year2023/Day23Solver.kt
@@ -0,0 +1,12 @@
+package com.cormontia.adventOfCode.year2023
+
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+class Day23Solver {
+    fun solve() {
+        val input = Path("""src/main/resources/inputFiles/AoCDay23_sample1.txt""")
+            .readLines()
+
+    }
+}
---
+++ src/main/kotlin/com/cormontia/adventOfCode/year2023/Day24Solver.kt
@@ -0,0 +1,12 @@
+package com.cormontia.adventOfCode.year2023
+
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+class Day24Solver {
+    fun solve() {
+        val input = Path("""src/main/resources/inputFiles/AoCDay24_sample1.txt""")
+            .readLines()
+
+    }
+}
