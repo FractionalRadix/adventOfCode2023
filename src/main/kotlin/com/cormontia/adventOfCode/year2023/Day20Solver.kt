c0de08248e0f39021a7d0600a8fac83db108868e
Serge <nerostjust@gmail.com>
2023-12-23 20:44:44 +0100

    Add skeleton code for day 20.

---
+++ src/main/kotlin/com/cormontia/adventOfCode/year2023/Day20Solver.kt
@@ -0,0 +1,11 @@
+package com.cormontia.adventOfCode.year2023
+
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+class Day20Solver {
+    fun solve() {
+        val input = Path("""src/main/resources/inputFiles/AoCDay20_sample1.txt""")
+            .readLines()
+    }
+}
