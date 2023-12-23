099478264d54985a0d92659a74eaad98cd0646b3
Serge <nerostjust@gmail.com>
2023-12-23 20:14:07 +0100

    Add partial solution for day 23, part 1.

--- src/main/kotlin/com/cormontia/adventOfCode/year2023/Day23Solver.kt
+++ src/main/kotlin/com/cormontia/adventOfCode/year2023/Day23Solver.kt
@@ -1,5 +1,7 @@
 package com.cormontia.adventOfCode.year2023
 
+import utils.Coor
+import utils.buildGridMap
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
@@ -8,5 +10,90 @@ class Day23Solver {
         val input = Path("""src/main/resources/inputFiles/AoCDay23_sample1.txt""")
             .readLines()
 
+        val grid = Grid(buildGridMap(input))
+        solvePart1(grid)
+
     }
+
+    fun solvePart1(grid: Grid) {
+        // Inefficient solution: depth-first search with backtracking.
+
+        val firstRow = grid.grid.filter { it.key.row == grid.minRow }
+        val firstColIdx = firstRow.filter { it.value == '.' }.minBy { it.key.col }.key.col
+        val lastRow = grid.grid.filter { it.key.row == grid.maxRow }
+        val lastColIdx = lastRow.filter { it.value == '.' }.maxBy { it.key.col }.key.col
+        val firstPos = Coor(grid.minRow, firstColIdx)
+        val lastPos = Coor(grid.maxRow, lastColIdx)
+        val visited = mutableListOf<Coor>()
+
+        step(grid, firstPos, visited, lastPos)
+    }
+
+    fun step(grid: Grid, pos: Coor, visited: MutableList<Coor>, goal: Coor) {
+        // paths (.), forest (#), and steep slopes (^, >, v, and <).
+        // Let's ignore the special rules for "slopes", and just treat them like "path".
+
+        val neighbours = grid.neighbours(pos)
+        val nextSteps = neighbours
+            .filter { it.value != '#' } // Only the tiles that you can move through.
+            .filter { it.key !in visited } // Only tiles you haven't visited yet.
+
+        val newList = mutableListOf<Coor>()
+        newList.addAll(visited)
+        newList.add(pos)
+        for (elt in nextSteps) {
+            if (elt.key == goal) {
+                println("Found! Length of path: ${newList.size}")
+            } else {
+                step(grid, elt.key, newList, goal)
+            }
+        }
+    }
+
+
+}
+
+//TODO?~ Move to utils package?
+class Grid(val grid: Map<Coor, Char>) {
+    val minRow = grid.keys.minBy { it.row }.row
+    val maxRow = grid.keys.maxBy { it.row }.row
+    val minCol = grid.keys.minBy { it.col }.col
+    val maxCol = grid.keys.maxBy { it.col }.col
+
+    /**
+     * Given a position on the grid, find the points left, right, above, and below that position.
+     * If any of these is outside the grid, ignore them.
+     * For example, if the given position is the top left corner, there would not be positions above or to the left.
+     * So in that case the result would be just the position below and the position to the right.
+     * @param position A position on that grid.
+     * @return The coordinates of the positions horizontally and vertically adjacent to the given position.
+     */
+    fun neighbours(position: Coor): Map<Coor, Char> {
+        val coordinates = mutableListOf<Coor>()
+
+        if (position.row > minRow) {
+            coordinates.add(Coor(position.row - 1, position.col))
+        }
+        if (position.row < maxRow) {
+            coordinates.add(Coor(position.row + 1, position.col))
+        }
+        if (position.col > minCol) {
+            coordinates.add(Coor(position.row, position.col - 1))
+        }
+        if (position.col < maxCol) {
+            coordinates.add(Coor(position.row, position.col + 1))
+        }
+
+        return grid.filter { it.key in coordinates }
+    }
+
+    fun print() {
+        for (row in minRow .. maxRow) {
+            println()
+            for (col in minCol .. maxCol) {
+                print(grid[Coor(row,col)])
+            }
+        }
+    }
+
 }
